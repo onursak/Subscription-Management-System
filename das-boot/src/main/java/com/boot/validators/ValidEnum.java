a5d91a9fdaf0f97a6f88e71bde53da008e0e711f
Onur <33655893+onursak@users.noreply.github.com>
2019-09-14 11:02:35 +0300

    Update ValidEnum.java

--- das-boot/src/main/java/com/boot/validators/ValidEnum.java
+++ das-boot/src/main/java/com/boot/validators/ValidEnum.java
@@ -10,14 +10,11 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 
 @Documented
-//Note: We use here already a validator which we will add in a sec too
 @Constraint(validatedBy = EnumValidator.class)
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnum {
-// used to get later in the resource bundle the i18n text
 String message() default "There is no enum type like this!";
 Class<?>[] groups() default {};
 Class<? extends Payload>[] payload() default {};
-// min value, we for now just a string
 }
